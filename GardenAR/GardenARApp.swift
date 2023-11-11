1944383de5e70a5dc70a5f843e9130b55e643055
Aashish Kapoor <kapooraashish02@gmail.com>
2023-11-10 21:43:52 -0800

    Initial Commit

---
+++ GardenAR/GardenARApp.swift
@@ -0,0 +1,17 @@
+//
+//  GardenARApp.swift
+//  GardenAR
+//
+//  Created by Aashish Kapoor on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GardenARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
