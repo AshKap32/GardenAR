53d8281e3dc3b6579f46111a05834a285df404f8
Anh Thu Pham <anhthu.pham@sjsu.edu>
2024-04-26 11:53:05 -0700

    created section dividers on registration view

--- GardenAR/Authentication/RegisterView.swift
+++ GardenAR/Authentication/RegisterView.swift
@@ -62,153 +62,183 @@ struct RegisterView: View {
         ZStack {
             Color("Colors/Alt")
                 .ignoresSafeArea()
-            
-            VStack(spacing: 12.0) {
-                HStack {
-                    Image(systemName: "person.fill")
-                        .frame(width: 24.0, alignment: .leading)
-                    
-                    TextField("Username", text: self.$username)
-                        .textInputAutocapitalization(.never)
-                        .autocorrectionDisabled()
-                }
-                .padding(12.0)
-                .background(.white)
-                .clipShape(.rect(cornerRadius: 6.0))
-                
-                HStack {
-                    Image(systemName: "person.fill")
-                        .frame(width: 24.0, alignment: .leading)
-                    
-                    TextField("Nickname", text: self.$nickname)
-                        .textInputAutocapitalization(.never)
-                        .autocorrectionDisabled()
-                }
-                .padding(12.0)
-                .background(.white)
-                .clipShape(.rect(cornerRadius: 6.0))
-                
-                HStack {
-                    Image(systemName: "person.fill")
-                        .frame(width: 24.0, alignment: .leading)
-                    
-                    TextField("First name", text: self.$forename)
-                        .textInputAutocapitalization(.never)
-                        .autocorrectionDisabled()
-                }
-                .padding(12.0)
-                .background(.white)
-                .clipShape(.rect(cornerRadius: 6.0))
-                
-                HStack {
-                    Image(systemName: "person.fill")
-                        .frame(width: 24.0, alignment: .leading)
-                    
-                    TextField("Last name", text: self.$surname)
-                        .textInputAutocapitalization(.never)
-                        .autocorrectionDisabled()
-                }
-                .padding(12.0)
-                .background(.white)
-                .clipShape(.rect(cornerRadius: 6.0))
-                
-                HStack {
-                    Image(systemName: "envelope.fill")
-                        .frame(width: 24.0, alignment: .leading)
-                    
-                    TextField("Email", text: self.$email)
-                        .textInputAutocapitalization(.never)
-                        .autocorrectionDisabled()
-                }
-                .padding(12.0)
-                .background(.white)
-                .clipShape(.rect(cornerRadius: 6.0))
-                
-                HStack {
-                    Image(systemName: "lock.fill")
-                        .frame(width: 24.0, alignment: .leading)
-                    
-                    SecureField("Password", text: self.$password)
-                        .textInputAutocapitalization(.never)
-                        .autocorrectionDisabled()
-                }
-                .padding(12.0)
-                .background(.white)
-                .clipShape(.rect(cornerRadius: 6.0))
-                
-                Menu {
-                    ForEach(self.skillOptions, id: \.self) { item in
-                        Button(item, action: {
-                            self.skill = item
-                        })
-                    }
-                } label : {
-                    Image(systemName: "rosette")
-                        .frame(width: 24.0, alignment: .leading)
-                        .foregroundColor(.black)
-                    
-                    TextField("Skill level", text: self.$skill)
-                        .disabled(true)
-                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
-                    
-                    Spacer()
-                    Image(systemName: "chevron.down")
-                        .foregroundColor(.black)
-                }
-                .padding(12.0)
-                .background(.white)
-                .clipShape(.rect(cornerRadius: 6.0))
-                
-                HStack {
-                    Image(systemName: "building.fill")
-                        .frame(width: 24.0, alignment: .leading)
-                    
-                    TextField("City", text: self.$city)
-                        .textInputAutocapitalization(.never)
-                        .autocorrectionDisabled()
-                }
-                .padding(12.0)
-                .background(.white)
-                .clipShape(.rect(cornerRadius: 6.0))
-                
-                HStack {
-                    Image(systemName: "building.fill")
-                        .frame(width: 24.0, alignment: .leading)
-                    
-                    TextField("Zip code", text: self.$zip)
-                        .textInputAutocapitalization(.never)
-                        .autocorrectionDisabled()
-                }
-                .padding(12.0)
-                .background(.white)
-                .clipShape(.rect(cornerRadius: 6.0))
-                
-                Button(action: {
-                    Task {
-                        await self.register()
-                    }
-                }) {
-                    Spacer()
-                    Text("Sign up")
-                        .foregroundStyle(.white)
-                    
-                    Spacer()
-                }
-                .padding(12.0)
-                .background(.black)
-                .clipShape(.rect(cornerRadius: 6.0))
+            ScrollView {
+                VStack(alignment: .leading, spacing: 12.0) {
+                    Text("Join GardenAR")
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .fontWeight(.bold)
 
-                HStack {
-                    Text("Already have an account?")
-                        .foregroundStyle(.white)
+                    Text("Personal Information")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding(.top, 5)
+                        .padding(.bottom, 5)
                     
-                    Button(action: self.toggle) {
-                        Text("Sign in")
-                            .foregroundStyle(.green)
+                    HStack {
+                        Image(systemName: "person.fill")
+                            .frame(width: 24.0, alignment: .leading)
+                        
+                        TextField("Username", text: self.$username)
+                            .textInputAutocapitalization(.never)
+                            .autocorrectionDisabled()
+                    }
+                    .padding(12.0)
+                    .background(.white)
+                    .clipShape(.rect(cornerRadius: 6.0))
+                    
+                    HStack {
+                        Image(systemName: "person.fill")
+                            .frame(width: 24.0, alignment: .leading)
+                        
+                        TextField("Nickname", text: self.$nickname)
+                            .textInputAutocapitalization(.never)
+                            .autocorrectionDisabled()
+                    }
+                    .padding(12.0)
+                    .background(.white)
+                    .clipShape(.rect(cornerRadius: 6.0))
+                    
+                    HStack {
+                        Image(systemName: "person.fill")
+                            .frame(width: 24.0, alignment: .leading)
+                        
+                        TextField("First name", text: self.$forename)
+                            .textInputAutocapitalization(.never)
+                            .autocorrectionDisabled()
+                    }
+                    .padding(12.0)
+                    .background(.white)
+                    .clipShape(.rect(cornerRadius: 6.0))
+                    
+                    HStack {
+                        Image(systemName: "person.fill")
+                            .frame(width: 24.0, alignment: .leading)
+                        
+                        TextField("Last name", text: self.$surname)
+                            .textInputAutocapitalization(.never)
+                            .autocorrectionDisabled()
+                    }
+                    .padding(12.0)
+                    .background(.white)
+                    .clipShape(.rect(cornerRadius: 6.0))
+                    
+                    Text("Account Information")
+                        .foregroundColor(.white)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .padding(.top, 10)
+                        .padding(.top, 5)
+                    HStack {
+                        Image(systemName: "envelope.fill")
+                            .frame(width: 24.0, alignment: .leading)
+                        
+                        TextField("Email", text: self.$email)
+                            .textInputAutocapitalization(.never)
+                            .autocorrectionDisabled()
+                    }
+                    .padding(12.0)
+                    .background(.white)
+                    .clipShape(.rect(cornerRadius: 6.0))
+                    
+                    HStack {
+                        Image(systemName: "lock.fill")
+                            .frame(width: 24.0, alignment: .leading)
+                        
+                        SecureField("Password", text: self.$password)
+                            .textInputAutocapitalization(.never)
+                            .autocorrectionDisabled()
+                    }
+                    .padding(12.0)
+                    .background(.white)
+                    .clipShape(.rect(cornerRadius: 6.0))
+                    
+                    Text("Gardening information")
+                        .foregroundColor(.white)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .padding(.top, 10)
+                        .padding(.top, 5)
+                    
+                    Menu {
+                        ForEach(self.skillOptions, id: \.self) { item in
+                            Button(item, action: {
+                                self.skill = item
+                            })
+                        }
+                    } label : {
+                        Image(systemName: "rosette")
+                            .frame(width: 24.0, alignment: .leading)
+                            .foregroundColor(.black)
+                        
+                        TextField("Skill level", text: self.$skill)
+                            .disabled(true)
+                            .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                        
+                        Spacer()
+                        Image(systemName: "chevron.down")
+                            .foregroundColor(.black)
+                    }
+                    .padding(12.0)
+                    .background(.white)
+                    .clipShape(.rect(cornerRadius: 6.0))
+                    
+                    Text("Location")
+                        .foregroundColor(.white)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .padding(.top, 10)
+                        .padding(.top, 5)
+                    HStack {
+                        Image(systemName: "building.fill")
+                            .frame(width: 24.0, alignment: .leading)
+                        
+                        TextField("City", text: self.$city)
+                            .textInputAutocapitalization(.never)
+                            .autocorrectionDisabled()
+                    }
+                    .padding(12.0)
+                    .background(.white)
+                    .clipShape(.rect(cornerRadius: 6.0))
+                    
+                    HStack {
+                        Image(systemName: "building.fill")
+                            .frame(width: 24.0, alignment: .leading)
+                        
+                        TextField("Zip code", text: self.$zip)
+                            .textInputAutocapitalization(.never)
+                            .autocorrectionDisabled()
+                    }
+                    .padding(12.0)
+                    .background(.white)
+                    .clipShape(.rect(cornerRadius: 6.0))
+                    
+                    Button(action: {
+                        Task {
+                            await self.register()
+                        }
+                    }) {
+                        Spacer()
+                        Text("Sign up")
+                            .foregroundStyle(.white)
+                        
+                        Spacer()
+                    }
+                    .padding(12.0)
+                    .background(.black)
+                    .clipShape(.rect(cornerRadius: 6.0))
+
+                    HStack {
+                        Text("Already have an account?")
+                            .foregroundStyle(.white)
+                        
+                        Button(action: self.toggle) {
+                            Text("Log in")
+                                .foregroundStyle(.green)
+                            
+                        }
                     }
                 }
+                .padding(.horizontal)
             }
-            .padding(.horizontal)
+            
         }
     }
 }
